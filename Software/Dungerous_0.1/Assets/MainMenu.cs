77a1b8962e7c4fe061db8a36ed368538425e36b5
s0567271 <s0567271@htw-berlin.de>
2020-06-21 21:21:37 +0200

    Updating main menu and adding SceneTransitions

--- Software/Dungerous_0.1/Assets/MainMenu.cs
+++ Software/Dungerous_0.1/Assets/MainMenu.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
-
 {
-   public void StartGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+
+    public Animator transitionAnime;
+
+    public void StartGame()
+     {
+         StartCoroutine(LoadScene());
+        
+     }
+
+     IEnumerator LoadScene()
+     {
+         transitionAnime.SetTrigger("end");
+         yield return new WaitForSeconds(1.5f);
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+     }
 }
