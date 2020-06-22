99d73fd95d9607d31a658b1f45ccdc99c4106a7f
s0567271 <s0567271@htw-berlin.de>
2020-06-22 15:21:20 +0200

    in-game menu (pause, quit) functionality is created

--- Software/Dungerous_0.1/Assets/PauseMenu.cs
+++ Software/Dungerous_0.1/Assets/PauseMenu.cs
@@ -9,9 +9,31 @@ public class PauseMenu : MonoBehaviour
     public GameObject pauseMenuUi;
     public Animator transitionAnime;
 
+    // TODO: Fix this function
+    public void QuitGame()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionAnime.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
     void Update()
     {
+        if (GameIsPaused)
+        {
+            resume();
+        }
+        else
+        {
+            pause();
+        }
 
+        /*
         void pauseGame()
         {
             if (GameIsPaused)
@@ -23,33 +45,21 @@ public class PauseMenu : MonoBehaviour
                 pause();
             }
         }
+        */
+        
+    }
 
+    public void resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
 
-        void resume()
-        {
-            pauseMenuUi.SetActive(false);
-            Time.timeScale = 1f;
-            GameIsPaused = false;
-        }
-
-        void pause()
-        {
-            pauseMenuUi.SetActive(true);
-            Time.timeScale = 0f;
-            GameIsPaused = true;
-        }
-
-        // TODO: Fix this function
-        void QuitGame()
-        {
-            StartCoroutine(LoadScene());
-        }
-
-        IEnumerator LoadScene()
-        {
-            transitionAnime.SetTrigger("end");
-            yield return new WaitForSeconds(1.5f);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-        }
+    public void pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
 }
