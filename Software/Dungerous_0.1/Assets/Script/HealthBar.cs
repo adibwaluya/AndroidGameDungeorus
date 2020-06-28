ad7d0a27d0e6bb94ef9dd177d0ee21e109dcc032
Minh Hoang Do <s0558411@htw-berlin.de>
2020-06-28 14:19:25 +0200

    Added Health bar to main scene and gradient

--- Software/Dungerous_0.1/Assets/Script/HealthBar.cs
+++ Software/Dungerous_0.1/Assets/Script/HealthBar.cs
@@ -7,14 +7,22 @@ public class HealthBar : MonoBehaviour
 {
     public Slider slider;
 
+    public Gradient gradient;
+
+    public Image fill;
+
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
