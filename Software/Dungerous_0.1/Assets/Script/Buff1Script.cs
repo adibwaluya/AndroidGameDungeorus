19c08274f8c2862082e80f4aa03fbe17d224eecc
Minh Hoang Do <s0558411@htw-berlin.de>
2020-07-05 15:59:54 +0200

    working on items

---
+++ Software/Dungerous_0.1/Assets/Script/Buff1Script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buff1Script : MonoBehaviour
+{
+
+
+    public Collider2D collision;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        OnTriggerEnter2D(collision);
+
+        //DestroyItem(collision);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void DestroyItem(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
