febcbf15e761fc4848e4c8c885002bd6631e6b13
Minh Hoang Do <s0558411@htw-berlin.de>
2020-07-06 17:12:06 +0200

    fixed item collision and added enemy movement

--- Software/Dungerous_0.1/Assets/Script/Buff1Script.cs
+++ Software/Dungerous_0.1/Assets/Script/Buff1Script.cs
@@ -5,40 +5,24 @@ using UnityEngine;
 public class Buff1Script : MonoBehaviour
 {
 
-    //TODO: To be activated once again to debug!
-
-    /*
     public Collider2D collision;
+    public GameObject item;
+
     // Update is called once per frame
     void FixedUpdate()
     {
         OnTriggerEnter2D(collision);
 
-        //DestroyItem(collision);
     }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag.Equals("Player"))
+        if(collision.gameObject.tag.Equals("Player"))
         {
-            Destroy(gameObject);
-        }
-
-        if (collision.gameObject.)
-        {
-            Destroy(gameObject);
+            Destroy(item);
         }
 
     }
-
-    private void DestroyItem(Collider2D collision)
-    {
-        if (collision.gameObject.tag.Equals("Player"))
-        {
-            Destroy(gameObject);
-        }
-    }
-    */
 
 }
--- Software/Dungerous_0.1/Assets/Script/Enemy.cs
+++ Software/Dungerous_0.1/Assets/Script/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,42 @@ public class Enemy : MonoBehaviour
 
     public GameObject DeathEffect;
 
+    public Transform Player;
+
+    public Rigidbody2D rb;
+
+    public float moveSpeed = 1f;
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Character").transform;
+    }
+
+    void Update()
+    {
+
+        //Getting the direction to the player
+        Vector3 direction = Player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle - 90f;
+        direction.Normalize();
+        movement = direction;
+    }
+
+
+    private void FixedUpdate()
+    {
+        moveEnemy(movement);
+    }
+
+    void moveEnemy(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
+    //Calculates enemy damage taken
     public void TakeDamage(int damage)
     {
         health -= damage;
@@ -17,6 +54,7 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    //Destroys enemy object
     void Die()
     {
         GameObject effect = Instantiate(DeathEffect, transform.position, Quaternion.identity);
