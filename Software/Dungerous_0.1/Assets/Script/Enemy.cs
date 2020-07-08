0051c5dd6e4d0105a3cc1b30c67206e1e656b16f
Minh Hoang Do <s0558411@htw-berlin.de>
2020-07-08 15:45:15 +0200

    updated main scene

--- Software/Dungerous_0.1/Assets/Script/Enemy.cs
+++ Software/Dungerous_0.1/Assets/Script/Enemy.cs
@@ -21,6 +21,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         Player = GameObject.FindWithTag("Character").transform;
     }
 
