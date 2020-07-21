786549da7d215b87ee899b9fc56f7e687b802e0a
s0567271 <s0567271@htw-berlin.de>
2020-07-21 23:51:04 +0200

    damage for punch

---
+++ Software/Dungerous_0.1/Assets/Script/PunchEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchEnemy : MonoBehaviour
+{
+    public int dmgToGive = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag.Equals("Enemy"))
+        {
+            Enemy enemyHealth;
+            enemyHealth = other.gameObject.GetComponent<Enemy>();
+            enemyHealth.TakeDamage(dmgToGive);
+        }
+    }
+}
