654f506853420a9e537087ac6764bc6df9d19741
Minh Hoang Do <s0558411@htw-berlin.de>
2020-07-21 16:34:55 +0200

    added nurse weapon and patroling enemies

---
+++ Software/Dungerous_0.1/Assets/Script/Patrol.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    public Transform[] moveSpots;
+    private int randomSpot;
+    private float waitTime;
+    public float startWaitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = startWaitTime;
+        randomSpot = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position =
+            Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+
+
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                if (randomSpot < moveSpots.Length)
+                {
+                    randomSpot += 1;
+                }
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+               
+            }
+        }
+
+        if (randomSpot == moveSpots.Length)
+        {
+            randomSpot = 0;
+        }
+    }
+}
