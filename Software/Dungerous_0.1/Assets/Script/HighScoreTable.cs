fdb5dec8c500a74332b407f4f7eca169b2755f7b
s0567271 <s0567271@htw-berlin.de>
2020-07-26 15:51:25 +0200

    setting up highscore board

---
+++ Software/Dungerous_0.1/Assets/Script/HighScoreTable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreTable : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+
+    private void Awake()
+    {
+        entryContainer = transform.Find("HighScoreEntryContainer");
+        entryTemplate = transform.Find("HighScoreEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        float templateHigh = 20f;
+        for (int i = 0; i < 10; i++)
+        {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHigh * i);
+            entryTransform.gameObject.SetActive(true);
+        }
+        
+    }
+}
