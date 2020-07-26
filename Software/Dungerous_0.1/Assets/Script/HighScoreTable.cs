1d9b3e7e36253a18fbdd86fe86e5bc59d793c031
s0567271 <s0567271@htw-berlin.de>
2020-07-26 17:47:35 +0200

    fixing highscore board

--- Software/Dungerous_0.1/Assets/Script/HighScoreTable.cs
+++ Software/Dungerous_0.1/Assets/Script/HighScoreTable.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HighScoreTable : MonoBehaviour
 {
@@ -10,17 +11,44 @@ public class HighScoreTable : MonoBehaviour
     private void Awake()
     {
         entryContainer = transform.Find("HighScoreEntryContainer");
-        entryTemplate = transform.Find("HighScoreEntryTemplate");
+        entryTemplate = entryContainer.Find("HighScoreEntryTemplate");
 
         entryTemplate.gameObject.SetActive(false);
 
-        float templateHigh = 20f;
-        for (int i = 0; i < 10; i++)
+        float templateHigh = 30f;
+        for (int i = 0; i < 8 ; i++)
         {
             Transform entryTransform = Instantiate(entryTemplate, entryContainer);
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
             entryRectTransform.anchoredPosition = new Vector2(0, -templateHigh * i);
             entryTransform.gameObject.SetActive(true);
+
+            int rank = i + 1;
+            string rankString;
+            switch (rank)
+            {
+                default:
+                    rankString = rank + "TH";
+                    break;
+
+                case 1:
+                    rankString = "1ST";
+                    break;
+                case 2:
+                    rankString = "2ND";
+                    break;
+                case 3:
+                    rankString = "3RD";
+                    break;
+
+            }
+            entryTransform.Find("PosHeader").GetComponent<Text>().text = rankString;
+
+            int score = Random.Range(0, 10000);
+            entryTransform.Find("ScoreHeader").GetComponent<Text>().text = score.ToString();
+
+            string name = "John";
+            entryTransform.Find("NameHeader").GetComponent<Text>().text = name;
         }
         
     }
