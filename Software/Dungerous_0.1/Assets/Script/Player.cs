562306eabdca42150c580e75541d019d6d60412a
s0567271 <s0567271@htw-berlin.de>
2020-07-29 12:29:37 +0200

    melee attack is now implemented

--- Software/Dungerous_0.1/Assets/Script/Player.cs
+++ Software/Dungerous_0.1/Assets/Script/Player.cs
@@ -52,10 +52,12 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if (collision.gameObject.tag.Equals("Enemy"))
         {
-            TakeDamage(10);
+            TakeDamage(5);
         }
+        
 
         if (collision.gameObject.tag.Equals("Heal"))
         {
--- Software/Dungerous_0.1/Assets/Script/PunchEnemy.cs
+++ Software/Dungerous_0.1/Assets/Script/PunchEnemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PunchEnemy : MonoBehaviour
 {
-    public int dmgToGive = 50;
+    public int dmgToGive;
     // Start is called before the first frame update
     void Start()
     {
